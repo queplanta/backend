7093494ef95cdeb808d1eb319cc028d6d8e14e17
Alisson Patricio <eu@alisson.net>
2016-08-19 13:29:12 -0300

    Overide AuthConfig of django.contrib.auth
    This is because our username is unique together with is_tip, it can't be unique alone because of the versioning system, so django.contrib.auth.checks.check_user_model should not check this default requirement.

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.core import checks
+from django.contrib.auth.apps import AuthConfig as DjangoAuthConfig
+
+from django.contrib.auth.management import create_permissions
+from .auth_checks import check_user_model
 
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+
+class AuthConfig(DjangoAuthConfig):
+    def ready(self):
+        post_migrate.connect(
+            create_permissions,
+            dispatch_uid="django.contrib.auth.management.create_permissions")
+        checks.register(check_user_model, checks.Tags.models)
