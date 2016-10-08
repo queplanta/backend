23b9bc623cc77b0256ecba1261dd18edff45e41f
Alisson Patricio <eu@alisson.net>
2016-10-07 22:57:59 -0300

    Add email or username authentication backend

---
+++ accounts/authentication.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import backends
+from django.db.models import Q
+from .models import User
+
+
+class ModelBackend(backends.ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(
+                Q(username__iexact=username) | Q(email__iexact=username))
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
