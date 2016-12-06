f168c3bc26f539b97725550a3f73d15f1e126db1
Alisson Patricio <eu@alisson.net>
2016-12-06 19:42:24 -0200

    Add comments to Page

--- pages/models_graphql.py
+++ pages/models_graphql.py
@@ -4,9 +4,10 @@ from graphene.relay import Node
 from db.types_revision import DocumentRevisionBase
 
 from .models import Page as PageModel
+from commenting.models_graphql import CommentsNode
 
 
-class Page(DocumentRevisionBase, DjangoObjectType):
+class Page(DocumentRevisionBase, CommentsNode, DjangoObjectType):
     class Meta:
         model = PageModel
         interfaces = (Node, )
