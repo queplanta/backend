e4bfe0072a487c06e8c29f87902e898337c58148
Gabriel Leandro <gabrieleandro.dev@gmail.com>
2021-07-29 16:40:23 -0300

    Add fields and resolve

--- occurrences/models.py
+++ occurrences/models.py
@@ -8,6 +8,31 @@ from db.fields import ManyToManyField, limit_by_contenttype
 from images.models import Image
 
 
+OCCURRANCE_TYPE_CHOICES = (
+    ('natural', _('Natural')),
+    ('planted', _('Planted')),
+)
+
+TRUNK_TYPE_CHOICES = (
+    ('straight', _('Straight')),
+    ('bifurcated', _('Bifurcated')),
+    ('crooked', _('Crooked')),
+)
+
+CANOPY_POSITION_CHOICES = (
+    ('emerging', _('Emerging')),
+    ('canopy', _('Canopy')),
+    ('sub-canopy', _('Sub-canopy')),
+    ('sub-forest', _('Sub-forest')),
+    ('isolated', _('Isolated')),
+)
+
+HEALTH_STATE_CHOICES = (
+    ('healthy', _('Healthy')),
+    ('damaged', _('Damaged')),
+)
+
+
 class Occurrence(DocumentBase):
     identity = models.ForeignKey(
         DocumentID, related_name="occurrence_identity",
@@ -29,6 +54,19 @@ class Occurrence(DocumentBase):
     notes = models.TextField(blank=True, null=True)
     is_cultivated = models.BooleanField(default=False)
     is_request = models.BooleanField(default=False)
+
+    # plant nursery fields
+    type = models.CharField(choices=OCCURRANCE_TYPE_CHOICES, max_length=25, null=True, blank=True)
+    regional_name = models.CharField(max_length=255, null=True, blank=True)
+    cap = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    dap = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    total_height = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    cup_height = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    canopy_position = models.CharField(choices=CANOPY_POSITION_CHOICES, max_length=25, null=True, blank=True)
+    current_health_state = models.CharField(choices=HEALTH_STATE_CHOICES, max_length=25, null=True, blank=True)
+    current_health_state_description = models.TextField(blank=True, null=True)
+    type_of_trunk = models.CharField(choices=TRUNK_TYPE_CHOICES, max_length=25, null=True, blank=True)
+    local_population = models.TextField(blank=True, null=True)
 
     REPUTATION_VALUE = 1
 
