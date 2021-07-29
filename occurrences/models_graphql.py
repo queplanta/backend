442eebc409f8fabac542de2930fc02488e77de51
Gabriel Leandro <gabrieleandro.dev@gmail.com>
2021-07-29 16:40:23 -0300

    Add fields and resolve

--- occurrences/models_graphql.py
+++ occurrences/models_graphql.py
@@ -12,13 +12,55 @@ from db.graphene import CountedConnection
 
 from .models import (
     Occurrence as OccurrenceModel,
-    Suggestion as SuggestionIDModel
+    Suggestion as SuggestionIDModel,
+    OCCURRANCE_TYPE_CHOICES, TRUNK_TYPE_CHOICES,
+    CANOPY_POSITION_CHOICES, HEALTH_STATE_CHOICES
 )
 from accounts.models_graphql import User
 from life.models_graphql import LifeNode
 from commenting.models_graphql import CommentsNode
 from voting.models_graphql import VotesNode
 from images.models_graphql import Image
+
+
+class OccurranceType(graphene.Enum):
+    NATURAL = 'natural'
+    PLANTED = 'planted'
+
+    @property
+    def description(self):
+        return dict(OCCURRANCE_TYPE_CHOICES)[self._value_]
+
+
+class TrunkType(graphene.Enum):
+    STRAIGHT = 'straight'
+    BIFURCATED = 'bifurcated'
+    CROOKED = 'crooked'
+
+    @property
+    def description(self):
+        return dict(TRUNK_TYPE_CHOICES)[self._value_]
+
+
+class CanopyPosition(graphene.Enum):
+   EMERGING = 'emerging'
+   CANOPY = 'canopy'
+   SUB_CANOPY = 'sub-canopy'
+   SUB_FOREST = 'sub-forest'
+   ISOLATED = 'isolated'
+
+    @property
+    def description(self):
+        return dict(CANOPY_POSITION_CHOICES)[self._value_]
+
+
+class HealthState(graphene.Enum):
+    HEALTHY = 'healthy'
+    DAMAGED = 'damaged'
+
+    @property
+    def description(self):
+        return dict(HEALTH_STATE_CHOICES)[self._value_]
 
 
 class OccurrenceCluster(graphene.ObjectType):
@@ -33,6 +75,22 @@ class Occurrence(DjangoObjectType, DocumentBase):
     answer = graphene.Field(lambda: SuggestionID)
     images = DjangoConnectionField(lambda: Image)
     identity = graphene.Field(LifeNode)
+
+    type = graphene.Field(OccurranceType)
+    typeDisplay = graphene.String()
+    regional_name = graphene.String()
+    cap = graphene.Decimal()
+    dap = graphene.Decimal()
+    total_height = graphene.Decimal()
+    cup_height = graphene.Decimal()
+    canopy_position = graphene.Field(CanopyPosition)
+    canopy_positionDisplay = graphene.String()
+    current_health_state = graphene.Field(HealthState)
+    current_health_stateDisplay = graphene.String()
+    current_health_state_description = graphene.String()
+    type_of_trunk = graphene.Field(TrunkType)
+    type_of_trunkDisplay = graphene.String()
+    local_population = graphene.String()
 
     class Meta:
         model = OccurrenceModel
@@ -66,6 +124,18 @@ class Occurrence(DjangoObjectType, DocumentBase):
                 select={'is_choosen_as_valid': "CASE WHEN identity_id = %d THEN 1 ELSE 0 END" % self.identity_id}
             ).order_by('-is_choosen_as_valid', '-document__votestats__sum_values')
         return qs.order_by('-document__votestats__sum_values')
+
+    def resolve_type_display(self, info):
+        return self.get_type_display()
+
+    def resolve_canopy_position_display(self, info):
+        return self.get_canopy_position_display()
+
+    def resolve_current_health_state_display(self, info):
+        return self.get_current_health_state_display()
+
+    def resolve_type_of_trunk_display(self, info):
+        return self.get_type_of_trunk_display()
 
 
 class BoundBoxFilter(django_filters.CharFilter):
